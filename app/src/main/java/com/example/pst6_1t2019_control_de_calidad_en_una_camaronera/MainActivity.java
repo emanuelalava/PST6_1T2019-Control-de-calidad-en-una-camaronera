2ff0beac6d9b3d8819540f3be0415c04cb651092
Jancarlo Valencia <jananval@espol.edu.ec>
2019-07-29 23:55:37 -0500

    Merge Rama 1

---
+++ app/src/main/java/com/example/pst6_1t2019_control_de_calidad_en_una_camaronera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pst6_1t2019_control_de_calidad_en_una_camaronera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
