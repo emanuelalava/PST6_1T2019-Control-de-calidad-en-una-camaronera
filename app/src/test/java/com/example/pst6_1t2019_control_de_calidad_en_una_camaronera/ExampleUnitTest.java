1d189ba7b3b8055d1878023e89b448876a666367
Winter Alava <walava@espol.edu.ec>
2019-07-29 23:49:10 -0500

    Commit Inicial

---
+++ app/src/test/java/com/example/pst6_1t2019_control_de_calidad_en_una_camaronera/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.pst6_1t2019_control_de_calidad_en_una_camaronera;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
